e9879dd84102560cdb88b612d4eeac09d66b2dda
Aleksey Volkov <aleksey.marantz@gmail.com>
2020-01-30 19:59:32 +0300

    Add terraform

---
+++ docker-monolith/infra/terraform/prod/main.tf
@@ -0,0 +1,26 @@
+provider "google" {
+  version = "~> 2.15.0"
+  project = var.project
+  region  = var.region
+}
+
+provider "null" {
+  version = "~> 2.1"
+}
+
+module "app" {
+  project         = var.project
+  app_name        = "${var.app_name}"
+  source          = "../modules/app"
+  public_key_path = "${var.public_key_path}"
+  zone            = "${var.zone}"
+  app_disk_image  = "${var.app_disk_image}"
+  private_key     = var.private_key
+}
+
+
+module "vpc" {
+  source        = "../modules/vpc"
+  zone          = "${var.zone}"
+  source_ranges = "${var.my_ip}"
+}
