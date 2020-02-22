080e7d2b7cc54bf419f43ad6a67d71a86f92e9c4
Aleksey Volkov <aleksey.marantz@gmail.com>
2020-02-22 12:57:34 +0300

    Test 4

--- reddit/status_container.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-CONTAINER_NAME="gitlab-reddit-gitlab-*"
-OLD="$(docker ps --all --quiet --filter=name="$CONTAINER_NAME")"
-if [ -n "$OLD" ]; then
-  echo "Container running" && docker stop $OLD
-fi
